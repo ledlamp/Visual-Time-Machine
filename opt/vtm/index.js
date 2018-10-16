dfcd4641d455a8e8aa4b6c4997ded57e2d9c2c34
Lamp <ledlamp89@gmail.com>
2018-10-16 16:38:42 -0700

    Capture only on console + some refactor

--- opt/vtm/index.js
+++ opt/vtm/index.js
@@ -1,27 +1,35 @@
-var child_process = require("child_process");
+var exec = require("child_process").execSync;
 var fs = require("fs");
 var crypto = require("crypto");
 var os = require("os");
 var Discord = require("discord.js");
 
 var config = require("./config");
+var whoiam = exec("id -un").toString().trim();
 
 var webhook = new Discord.WebhookClient(config.webhook.id, config.webhook.token);
 
-(function capture() {
-	var filename = `${new Date().toISOString()} ${encodeURIComponent(crypto.randomBytes(16).toString('base64'))}.jpg`
-	var path = os.tmpdir() + '/' + filename;
-	try {
-		child_process.execSync(`screencapture -x -C -t jpg "${path}"`);
+function capture() {
+	return new Promise(function(resolve, reject){
+		if (config.capture_console_only) {
+			if (exec("stat -f '%Su' /dev/console").toString().trim() != whoiam) return reject("Not console");
+		}
+		var filename = `${new Date().toISOString()} ${encodeURIComponent(crypto.randomBytes(16).toString('base64'))}.jpg`
+		var path = os.tmpdir() + '/' + filename;
+		exec(`screencapture -x -C -t jpg "${path}"`);
 		var data = fs.readFileSync(path);
 		webhook.send({files:[{
 			attachment: data,
 			name: filename
 		}]}).finally(()=>{
 			fs.unlink(path, ()=>{});
-			setTimeout(capture, config.interval);
+			resolve();
 		});
-	} catch(e) {
-		setTimeout(capture, config.interval);
-	}
+	});
+}
+
+(function loop(){
+	capture().finally(()=>{
+		setTimeout(loop, config.interval);
+	}).catch(()=>{});
 })();
