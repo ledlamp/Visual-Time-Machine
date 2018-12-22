93716017ad64e90b34b4968443407f395c25f723
Lamp <ledlamp89@gmail.com>
2018-12-21 17:58:43 -0800

    Windows support

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ var os = require("os");
 var Discord = require("discord.js");
 
 var config = require("./config");
-var whoiam = exec("id -un").toString().trim();
+if (process.platform == "darwin" && config.capture_console_only) {
+	var whoiam = exec("id -un").toString().trim();
+}
 
 var webhook = new Discord.WebhookClient(config.webhook.id, config.webhook.token);
 
 async function capture() {
-	if (config.capture_console_only && exec("stat -f '%Su' /dev/console").toString().trim() != whoiam) throw "Not console";
-	var filename = `${new Date().toISOString()} ${encodeURIComponent(crypto.randomBytes(16).toString('base64'))}.jpg`
+	if (process.platform == "darwin" && config.capture_console_only && exec("stat -f '%Su' /dev/console").toString().trim() != whoiam) throw "Not console";
+	var filename = `${new Date().toISOString().replace(/:/g, '-')}_${encodeURIComponent(crypto.randomBytes(16).toString('base64'))}.jpg`
 	var filepath = os.tmpdir() + '/' + filename;
-	exec(`screencapture -x -C -t jpg "${filepath}"`);
+	if (process.platform == "darwin") {
+		exec(`screencapture -x -C -t jpg "${filepath}"`);
+	} else if (process.platform == "win32") {
+		exec(`"${__dirname}/screenCapture.exe" "${filepath}"`, {windowsHide: true});
+	} else {
+		console.error("Platform not supported!"); process.exit(1);
+	}
 	var data = fs.readFileSync(filepath);
 	try {
 		await webhook.send({files:[{
