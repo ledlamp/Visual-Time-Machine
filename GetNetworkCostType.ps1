42d3d4be6f76bb5cdb750e3608876768e9b27ab6
Lamp <ledlamp89@gmail.com>
2020-01-30 18:10:42 -0800

    Don't capture on metered networks

---
+++ GetNetworkCostType.ps1
@@ -0,0 +1,5 @@
+﻿[void][Windows.Networking.Connectivity.NetworkInformation,Windows,ContentType=WindowsRuntime]
+$connectionProfile=[Windows.Networking.Connectivity.NetworkInformation]::GetInternetConnectionProfile()
+$connectionCost=$connectionProfile.GetConnectionCost()
+$networkCostType=$connectionCost.NetworkCostType
+Write-Output $networkCostType
--- vtm.js
+++ vtm.js
@@ -10,11 +10,19 @@ var webhook = new Discord.WebhookClient(config.webhook.id, config.webhook.token)
 async function capture() {
 	if (process.platform == "darwin" && config.capture_console_only && exec("stat -f '%Su' /dev/console").toString().trim() != os.userInfo().username) throw "Not console";
 	if (process.platform == "win32") {
-		try {
+		try { //TODO doesn't work on home versions of windows?
 			exec("query user", {windowsHide: true}); // fsr this exits with code 1 which node considers an error
 		} catch(cp) {
-			if (cp.stdout.toString().split('\n').find(x => x.startsWith('>')).substr(46, 4) == "Disc") throw "Inactive session"; // will capture black screen otherwise
+			let x = cp.stdout.toString().split('\n').find(x => x.startsWith('>'));
+			if (x && x.substr(46, 4) == "Disc") throw "Inactive session"; // will capture black screen otherwise
 		}
+		try {
+			let output = String(exec("powershell ./GetNetworkCostType.ps1", {windowsHide: true})).trim();
+			if (output == "Fixed") throw "On metered network";
+			else if (output != "Unrestricted") {
+				console.warn("Unknown output from powershell script:", output);
+			}
+		} catch(e) { console.error(error.message); }
 	}
 	var filedate = new Date();
 	var filename = `${filedate.toISOString().replace(/:/g, '-')}_${encodeURIComponent(crypto.randomBytes(16).toString('base64'))}.jpg`
