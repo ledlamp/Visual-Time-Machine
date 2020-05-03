e7bbcd859e128e146aeed7714c40d74d72c9b7cc
Lamp <ledlamp89@gmail.com>
2020-05-03 16:46:14 -0700

    Use execFile

--- vtm.js
+++ vtm.js
@@ -1,4 +1,4 @@
-var exec = require("child_process").execSync;
+var exec = require("child_process").execFileSync;
 var fs = require("fs");
 var crypto = require("crypto");
 var os = require("os");
@@ -8,10 +8,10 @@ var config = require("./config");
 var webhook = new Discord.WebhookClient(config.webhook.id, config.webhook.token);
 
 async function capture() {
-	if (process.platform == "darwin" && config.capture_console_only && exec("stat -f '%Su' /dev/console").toString().trim() != os.userInfo().username) throw "Not console";
+	if (process.platform == "darwin" && config.capture_console_only && exec("stat",  ["-f", "%Su", "/dev/console"]).toString().trim() != os.userInfo().username) throw "Not console";
 	if (process.platform == "win32") {
-		try { //TODO doesn't work on home versions of windows?
-			exec("query user", {windowsHide: true}); // fsr this exits with code 1 which node considers an error
+		try { //TODO requires pro or enterprise
+			exec("query", ["user"], {windowsHide: true}); // fsr this exits with code 1 which node considers an error
 		} catch(cp) {
 			let x = cp.stdout.toString().split('\n').find(x => x.startsWith('>'));
 			if (x && x.substr(46, 4) == "Disc") throw "Inactive session"; // will capture black screen otherwise
@@ -19,8 +19,8 @@ async function capture() {
 		{
 			let output;
 			try {
-				output = String(exec("powershell ./GetNetworkCostType.ps1", {windowsHide: true})).trim();
-			} catch(e) { console.error(e.message); }
+				output = String(exec("powershell.exe", ["./GetNetworkCostType.ps1"], {windowsHide: true})).trim();
+			} catch(e) { console.error("powershell error:", e.message); }
 			if (output)
 			if (output == "Fixed") throw "On metered network";
 			else if (output == "Null") throw "No network";
@@ -33,11 +33,11 @@ async function capture() {
 	var filename = `${filedate.toISOString().replace(/:/g, '-')}_${encodeURIComponent(crypto.randomBytes(16).toString('base64'))}.jpg`
 	var filepath = os.tmpdir() + '/' + filename;
 	if (process.platform == "darwin") {
-		exec(`screencapture -x -C -t jpg "${filepath}"`);
+		exec("screencapture", ["-x", "-C", "-t", "jpg", filepath]);
 	} else if (process.platform == "win32") {
-		exec(`"${__dirname}/screenCapture.exe" "${filepath}"`, {windowsHide: true});
+		exec(`${__dirname}/screenCapture.exe`, [filepath], {windowsHide: true});
 	} else {
-		exec(`import -silent -window root "${filepath}"`);
+		exec("import", ["-silent", "-window", "root", filepath]);
 	}
 	var data = fs.readFileSync(filepath);
 	try {
